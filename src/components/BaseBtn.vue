d07d0295220668c8dd64bb23040cdd6ecaffbe5f
Pop <gm@georgemiao.com>
2020-08-25 09:24:40 +0800

    🐞fix(main.vue): error parsing MS

--- src/components/BaseBtn.vue
+++ src/components/BaseBtn.vue
@@ -1,38 +1,50 @@
 <template>
-    <a class="btn" @click="$play(url)">
-        {{ $loc(btnName) }}
-    </a>
+<a class="btn" @click="$play(url)">
+    {{ name }}
+</a>
 </template>
+
 <script>
-    export default {
-        name: "BaseButton",
-        props: ["url", "btnName"]
+export default {
+    name: "BaseButton",
+    props: ["url", "btnName"],
+    computed: {
+        name() {
+            try {
+                return this.$loc(this.btnName)
+            } catch (e) {
+                console.log(e.message, this.btnName, this.url)
+                return null
+            }
+        }
     }
+}
 </script>
+
 <style>
-    .btn {
-        /*   Neuromophism design   */
-        border-radius: 16px;
-        background: var(--btn-color);
-        box-shadow: var(--box-shadow);
-        /* Neuromophism design ends*/
-        padding: 1em;
-        height: 1.2em;
-        min-width: 5rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        transition: all 0.1s ease;
-        margin: 0.8em;
-        font-size: 1rem;
-        cursor: pointer;
-    }
+.btn {
+    /*   Neuromophism design   */
+    border-radius: 16px;
+    background: var(--btn-color);
+    box-shadow: var(--box-shadow);
+    /* Neuromophism design ends*/
+    padding: 1em;
+    height: 1.2em;
+    min-width: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.1s ease;
+    margin: 0.8em;
+    font-size: 1rem;
+    cursor: pointer;
+}
 
-    .btn:active {
-        transform: scale(1.05, 1.05);
-    }
+.btn:active {
+    transform: scale(1.05, 1.05);
+}
 
-    .btn:hover {
-        box-shadow: var(--hover-shadow)
-    }
+.btn:hover {
+    box-shadow: var(--hover-shadow)
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,54 @@
 <template>
-  <div class="home">
+<div class="home">
     <a v-if="loading">Loading</a>
     <Panel v-for="(category, index) in categories" :key='index' :clips='category.clips' :category='category.name'>
     </Panel>
-  </div>
+</div>
 </template>
 
 <script>
-  import Panel from '@/components/Panel.vue'
-  export default {
+import Panel from '@/components/Panel.vue'
+export default {
     name: 'Home',
     components: {
-      Panel
+        Panel
+    },
+    async created() {
+        const loaded = (await this.$a.get(this.$store.getters.metaSource)).data
+        this.categories = this.parseCategory(loaded)
     },
     data() {
-      return {
-        loading: true
-      }
+        return {
+            loading: true,
+            categories: null
+        }
     },
     methods: {
-      parseCategory(loaded){
-        Object.entries(loaded.categories).forEach(([k, v]) => {
-          loaded.categories[k] = {
-            name: v,
-            clips: []
-          }
-        })
-        Object.values(loaded.clips).forEach(v => {
-          loaded.categories[v.category].clips.push(v)
-        })
-        return Object.values(loaded.categories)
-      }
-    },
-    asyncComputed: {
-      async categories() {
-        const loaded = (await this.$a.get(this.$store.getters.metaSource)).data
-        this.loading = false
-        /*
-        loaded[0] = {
-          clips: [],
-          catId: 'test',
-          name: {
-            en: 'test'
-          }
+        parseCategory(loaded) {
+            Object.entries(loaded.categories).forEach(([k, v]) => {
+                loaded.categories[k] = {
+                    name: v,
+                    clips: []
+                }
+            })
+            Object.entries(loaded.clips).forEach(([k, v]) => {
+                if (typeof v.category !== 'string') v.category = v.category[0]
+                v.name = v.name ?? {
+                    en: k
+                }
+                loaded.categories[v.category].clips.push(v)
+            })
+            return Object.values(loaded.categories)
         }
-        // A mp3 clip borrowed from suisei.moe for test purpose
-        loaded[0].clips.push({
-          name: {
-            en: "test"
-          },
-          url: "https://suisei.moe/assets/doooooooooo.mp3"
-        })
-        */
-        return this.parseCategory(loaded)
-      }
     }
-  }
-  
+}
 </script>
+
 <style>
-  .home {
+.home {
     display: flex;
     flex-wrap: wrap;
     width: auto;
     flex-direction: column;
-  }
+}
 </style>
